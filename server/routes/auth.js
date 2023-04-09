cac73ad63cefe42e5f77792a72685ce42f42c647
tyeb11 <tayyeb.merchant@gmail.com>
2023-04-09 19:05:11 +0530

    auth(add):login generateOTP verifyOTP verifyUser

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,17 @@
 import { Router } from "express";
-import { register } from "../controllers/auth.js";
-
+import {
+  register,
+  verifyUser,
+  login,
+  generateOTP,
+  verifyOTP,
+} from "../controllers/auth.js";
 const authRouter = Router();
 
 authRouter.post("/register", register);
+authRouter.post("/verify", verifyUser);
+authRouter.post("/login", verifyUser, login);
+authRouter.post("/generate-otp", generateOTP);
+authRouter.post("/verify-otp", verifyOTP);
 
 export default authRouter;
