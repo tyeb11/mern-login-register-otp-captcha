4ed8a7105811465544454224e185db200476b1d4
tyeb11 <tayyeb.merchant@gmail.com>
2023-04-10 18:14:37 +0530

    admin(add):register login get edit delete

--- server/routes/admin.js
+++ server/routes/admin.js
@@ -1,14 +1,20 @@
 import { Router } from "express";
+import {
+  deleteSlot,
+  editSlot,
+  getAllUsers,
+  login,
+  register,
+} from "../controllers/admin.js";
+import { verifyAdmin } from "../middlewares/verifyAdmin.js";
 
 const adminRouter = Router();
 
 adminRouter.get("/:user");
-adminRouter.get("/all-user");
-adminRouter.post("/login");
-adminRouter.post("/state");
-adminRouter.post("/city");
-adminRouter.post("/subject");
-adminRouter.post("/test-date");
-adminRouter.post("/time-slot");
+adminRouter.get("/all-user", verifyAdmin, getAllUsers);
+adminRouter.post("/login", verifyAdmin, login);
+adminRouter.post("/register", register);
+adminRouter.patch("/slot", verifyAdmin, editSlot);
+adminRouter.delete("/slot", verifyAdmin, deleteSlot);
 
 export default adminRouter;
