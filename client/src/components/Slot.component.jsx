634f01b3da5fc854c909baf8ef19137b2a423396
tyeb11 <tayyeb.merchant@gmail.com>
2023-04-11 19:22:13 +0530

    Slot(add):init

--- client/src/components/Slot.component.jsx
+++ client/src/components/Slot.component.jsx
@@ -1,5 +1,35 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Card from "react-bootstrap/Card";
+
 function Slot() {
-  return <></>;
+  const [slotData, setSlotData] = useState([]);
+  useEffect(() => {
+    async function getData() {
+      const { data } = await axios.get("/api/slot");
+      setSlotData(data.slots);
+    }
+    getData();
+  }, []);
+  return (
+    <>
+      {slotData.map((value) => {
+        return (
+          <>
+            <Card style={{ width: "10rem" }}>
+              <Card.Body>
+                <Card.Title>Subject {value.subject}</Card.Title>
+                <Card.Subtitle>Test Date {value.test_date}</Card.Subtitle>
+                <Card.Subtitle>Time Slot {value.time_slot}</Card.Subtitle>
+                <Card.Subtitle>City {value.city}</Card.Subtitle>
+                <Card.Subtitle>State {value.state}</Card.Subtitle>
+              </Card.Body>
+            </Card>
+          </>
+        );
+      })}
+    </>
+  );
 }
 
 export default Slot;
