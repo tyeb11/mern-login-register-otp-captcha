97e3fd8dbb622cfce534b264513a5c63157fdded
tyeb11 <tayyeb.merchant@gmail.com>
2023-04-11 17:29:45 +0530

    App(add):register verify-otp add-slot slot

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,5 +1,25 @@
-const App = () => {
-  return <>Hello</>;
-};
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import RegisterRoute from "./routes/user/Register.routes";
+import { Container } from "react-bootstrap";
+import VerifyOtpRoutes from "./routes/user/VerifyOtp.routes";
+import AddSlotRoute from "./routes/user/AddSlot.routes";
+import SlotRoute from "./routes/user/Slot.routes";
+
+function App() {
+  return (
+    <>
+      <Container>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/register" element={<RegisterRoute />} />
+            <Route path="/verify-otp" element={<VerifyOtpRoutes />} />
+            <Route path="/add-slot" element={<AddSlotRoute />} />
+            <Route path="slot" element={<SlotRoute />} />
+          </Routes>
+        </BrowserRouter>
+      </Container>
+    </>
+  );
+}
 
 export default App;
