acab649c94c8145b4fe10a5c66ae6fe4a68b1d70
tyeb11 <tayyeb.merchant@gmail.com>
2023-04-12 14:51:43 +0530

    App(add):admin routes

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,6 +4,7 @@ import { Container } from "react-bootstrap";
 import VerifyOtpRoutes from "./routes/user/VerifyOtp.routes";
 import AddSlotRoute from "./routes/user/AddSlot.routes";
 import SlotRoute from "./routes/user/Slot.routes";
+import Admin from "./routes/admin";
 
 function App() {
   return (
@@ -11,10 +12,11 @@ function App() {
       <Container>
         <BrowserRouter>
           <Routes>
-            <Route path="/register" element={<RegisterRoute />} />
-            <Route path="/verify-otp" element={<VerifyOtpRoutes />} />
-            <Route path="/add-slot" element={<AddSlotRoute />} />
-            <Route path="/slot" element={<SlotRoute />} />
+            <Route path="/register" index element={<RegisterRoute />} />
+            <Route path="/verify-otp" index element={<VerifyOtpRoutes />} />
+            <Route path="/add-slot" index element={<AddSlotRoute />} />
+            <Route path="/slot" index element={<SlotRoute />} />
+            <Route path="/admin/*" element={<Admin />} />
           </Routes>
         </BrowserRouter>
       </Container>
