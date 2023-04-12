ac252629db955bb9ec3988eac18e7027d0ea4a37
tyeb11 <tayyeb.merchant@gmail.com>
2023-04-12 14:52:27 +0530

    setupProxy(add):data admin

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,7 +1,18 @@
 const { createProxyMiddleware } = require("http-proxy-middleware");
 module.exports = function (app) {
   app.use(
-    ["/api/auth/register"],
+    [
+      "/api/auth/register",
+      "/api/auth/verify-otp",
+      "/api/data/state",
+      "/api/data/subject",
+      "/api/data/city",
+      "/api/data/test-date",
+      "/api/data/time-slot",
+      "/api/slot",
+      "/api/admin/register",
+      "/api/admin/all-user",
+    ],
     createProxyMiddleware({
       target: "http://localhost:3001",
     })
