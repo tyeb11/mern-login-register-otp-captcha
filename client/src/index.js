87634d12811c779270c78212513c0fd2b8eda367
tyeb11 <tayyeb.merchant@gmail.com>
2023-04-21 12:27:22 +0530

    index(add):bootstrap fonts

--- client/src/index.js
+++ client/src/index.js
@@ -1,4 +1,5 @@
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./styles/index.css";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App";
