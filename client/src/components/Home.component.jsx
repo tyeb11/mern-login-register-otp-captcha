8d606278e1995649f0056d1f1da19a15916f5847
tyeb11 <tayyeb.merchant@gmail.com>
2023-04-21 12:25:38 +0530

    home(add):styles

--- client/src/components/Home.component.jsx
+++ client/src/components/Home.component.jsx
@@ -1,13 +1,21 @@
 import { Button } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import { SubmitButton, EditButton } from "../styles/Button.styles";
 
 function Home() {
   const navigate = useNavigate();
   return (
     <>
       <h1>Book your Exam Slot here</h1>
-      <Button onClick={() => navigate("/register")}>Register</Button>
-      <Button onClick={() => navigate("/login")}>Login</Button>
+      <div style={{ display: "flex", gap: "20px", marginTop: "20px" }}>
+        <EditButton onClick={() => navigate("/login")}>Login</EditButton>
+        <SubmitButton
+          style={{ width: "150px" }}
+          onClick={() => navigate("/register")}
+        >
+          Register
+        </SubmitButton>
+      </div>
     </>
   );
 }
