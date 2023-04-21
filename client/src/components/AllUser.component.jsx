2ac8b5902f33da4f0c83256a4d504ff64a90c5a5
tyeb11 <tayyeb.merchant@gmail.com>
2023-04-21 12:23:34 +0530

    allUser(add):styles

--- client/src/components/AllUser.component.jsx
+++ client/src/components/AllUser.component.jsx
@@ -3,6 +3,10 @@ import Cookies from "js-cookie";
 import axios from "axios";
 import { Card } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
+import { BiCalendar, BiTimeFive } from "react-icons/bi";
+import { FaCheck } from "react-icons/fa";
+import { ImCross } from "react-icons/im";
+import { AiOutlineUser } from "react-icons/ai";
 
 function AllUser() {
   const navigate = useNavigate();
@@ -27,22 +31,44 @@ function AllUser() {
           <>
             <Card
               onClick={() => navigate(`/admin/slot/${value._id}`)}
-              style={{ width: "80rem" }}
+              style={{ width: "100%", margin: "20px", cursor: "pointer" }}
             >
-              <Card.Body>
-                <Card.Title>Name {value.candidate.name}</Card.Title>
-                <Card.Title>Email {value.candidate.email}</Card.Title>
-                <Card.Subtitle>
-                  User Type {value.candidate.user_type}
+              <Card.Body
+                style={{
+                  width: "100%",
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "center",
+                  position: "relative",
+                }}
+              >
+                <Card.Title style={{ marginTop: "20px", fontSize: "2rem" }}>
+                  {value.candidate.name}
+                </Card.Title>
+                <Card.Title>{value.candidate.email}</Card.Title>
+                <Card.Subtitle
+                  style={{ position: "absolute", left: "5px", bottom: "5px" }}
+                >
+                  <AiOutlineUser /> {value.candidate.user_type}
                 </Card.Subtitle>
-                <Card.Subtitle>
-                  Email Verified {value.candidate.email_verified ? "yes" : "no"}
+                <Card.Subtitle
+                  style={{ position: "absolute", right: "5px", bottom: "5px" }}
+                >
+                  Email Verified{" "}
+                  {value.candidate.email_verified ? <FaCheck /> : <ImCross />}
                 </Card.Subtitle>
                 <Card.Title>Subject {value.subject}</Card.Title>
-                <Card.Subtitle>Test Date {value.test_date}</Card.Subtitle>
-                <Card.Subtitle>Time Slot {value.time_slot}</Card.Subtitle>
-                <Card.Subtitle>City {value.city}</Card.Subtitle>
-                <Card.Subtitle>State {value.state}</Card.Subtitle>
+                <Card.Subtitle style={{ position: "absolute", right: "5px" }}>
+                  <BiCalendar />
+                  {value.test_date}
+                </Card.Subtitle>
+                <Card.Subtitle style={{ position: "absolute", left: "5px" }}>
+                  <BiTimeFive />
+                  {value.time_slot}
+                </Card.Subtitle>
+                <Card.Subtitle>
+                  {value.state},{value.city}
+                </Card.Subtitle>
               </Card.Body>
             </Card>
           </>
